1c88be886a993fc8c5a92e5c791c3537916985b0
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2025-01-09 18:53:38 +0600

    updated project model

--- models/project.js
+++ models/project.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 const projectSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  slug: { type: String, required: true },
   html: {
     type: String,
     required: true,
