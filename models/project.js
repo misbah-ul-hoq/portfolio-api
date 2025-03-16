7832f684dc9df03477027fe3115125c73d7723b3
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-03-16 22:50:38 +0600

    updated projectschema with a subTitle

--- models/project.js
+++ models/project.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const projectSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  subTitle: { type: String, required: true },
   slug: { type: String, required: true },
   thumbnail: { type: String, required: true },
   liveUrl: { type: String, required: true },
